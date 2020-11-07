421fa5a89baed9e0234b4e73b758ca14efdc740e
Johannes Deml <public@deml.io>
2020-11-07 12:34:53 +0100

    Add safezonehub script

---
+++ Assets/Scripts/SafeZoneHub.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Hub.cs">
+//   Copyright (c) 2020 Johannes Deml. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+
+public class SafeZoneHub: MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystemForceField forceField = null;
+
+    [SerializeField]
+    private SafeZoneData data = null;
+
+    [SerializeField]
+    private bool activated = false;
+
+    private float engeryLeft;
+
+    private void Awake()
+    {
+        forceField.endRange = data.Size;
+        engeryLeft = data.StartEnergy;
+    }
+
+    private void Start()
+    {
+        SetHubActive(activated);
+    }
+
+    private void SetHubActive(bool active)
+    {
+        forceField.gameObject.SetActive(active);
+    }
+}
