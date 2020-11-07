7e0d270a364ca664caab203c380c0c3ddcdb5e0f
Johannes Deml <public@deml.io>
2020-11-07 15:04:45 +0100

    Add progressbar to visualize energy

---
+++ Assets/Scripts/Ui/EnergyUi.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnergyUi.cs">
+//   Copyright (c) 2020 Johannes Deml. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Supyrb;
+using UnityEngine;
+
+public class EnergyUi: MonoBehaviour
+{
+	[SerializeField]
+	private RectTransform progressbar = null;
+
+	private PlayerEnergyLevelChangedSignal playerEnergyLevelChangedSignal;
+	private Vector2 anchorMax;
+
+	private void Awake()
+	{
+		anchorMax = progressbar.anchorMax;
+        
+		Signals.Get(out playerEnergyLevelChangedSignal);
+
+		playerEnergyLevelChangedSignal.AddListener(OnLevelChanged);
+	}
+
+	private void OnDestroy()
+	{
+		playerEnergyLevelChangedSignal.RemoveListener(OnLevelChanged);
+	}
+
+	private void OnLevelChanged(float level)
+	{
+		anchorMax.x = level;
+		progressbar.anchorMax = anchorMax;
+	}
+}
