7a59540d8a92608f8d5295fd0d6395513af28cf7
Johannes Deml <public@deml.io>
2020-11-07 13:23:13 +0100

    Add signals - wip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,41 @@
 using System;
+using Supyrb;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerData _playerData = null;
+
+    private float _energy;
+    private float _water;
     private Vector2 _moveVector;
     private Rigidbody _rigidbody;
+
+    private PlayerEnteredSafeZoneSignal _enterSafeZoneSignal;
+    private PlayerEnteredSafeZoneSignal _exitSafeZoneSignal;
+
+    private void Awake()
+    {
+        Signals.Get(out _enterSafeZoneSignal);
+        Signals.Get(out _exitSafeZoneSignal);
+        
+    }
+
+    private void Start()
+    {
+        _energy = _playerData.StartEnergy;
+        _water = _playerData.StartWater;
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _enterSafeZoneSignal.AddListener(OnEnterSafeZone);
+        _exitSafeZoneSignal.AddListener(OnExitSafeZone);
+    }
 
     private void FixedUpdate()
     {
         var moveDelta = _moveVector * (_playerData.MovementSpeed * Time.deltaTime);
         _rigidbody.velocity += new Vector3(moveDelta.x, 0, moveDelta.y);
-    }
-
-    private void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -48,6 +67,17 @@ public class PlayerController : MonoBehaviour
         }
 
         Debug.Log("Menu Action Pressed");
+    }
+    
+    
+    private void OnEnterSafeZone(SafeZoneHub hub)
+    {
+        Debug.Log("Entered safe zone");
+    }
+
+    private void OnExitSafeZone(SafeZoneHub hub)
+    {
+        Debug.Log("Exit safe zone");
     }
     
     private void Interact()
---
+++ Assets/Scripts/Signals/PlayerEnteredSafeZoneSignal.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayerEnteredSafeZone.cs">
+//   Copyright (c) 2020 Johannes Deml. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Supyrb;
+
+public class PlayerEnteredSafeZoneSignal: Signal<SafeZoneHub>
+{
+    
+}
